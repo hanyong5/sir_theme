22b6dfc45544e97a908bb2968df26ba18b714314
hanyong5 <hanyong5@naver.com>
2021-02-05 10:54:56 +0900

    g5 theme

--- basic/location.php
+++ basic/location.php
@@ -2,13 +2,19 @@
 include_once('./_common.php');
 if (!defined('_GNUBOARD_')) exit; // 개별 페이지 접근 불가
 
-$co_id = "testpage";
+$co_id = "location";
 $menuNum = "1";
 $menuNum2 = "2";
 $g5['title'] = "오시는길";
 
 include_once(G5_THEME_PATH.'/head.php');
 ?>
+<script>
+$(document).ready(function(){
+	$('#snb > li:nth-child(<?php echo $menuNum; ?>)').addClass("co_id<?php echo $co_id; ?> active");
+	$('#snb > li:nth-child(<?php echo $menuNum; ?>) > ul > li:nth-child(<?php echo $menuNum2; ?>)').addClass("snb2d_co_id<?php echo $co_id; ?>  active");
+	});
+</script>
 <!-- * 카카오맵 - 지도퍼가기 -->
 <!-- 1. 지도 노드 -->
 <div id="daumRoughmapContainer1612328491012" class="root_daum_roughmap root_daum_roughmap_landing"></div>
